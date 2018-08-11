cf98ebb5bfbb3e4d9f057e178b08602c0e7b16a1
Jim <jimbokururu27@gmail.com>
2018-08-11 14:18:44 +0800

    add test_fieldset

--- tests/test_fieldset.py
+++ tests/test_fieldset.py
@@ -2,8 +2,18 @@ import pytest
 import numpy as np
 from CGS_parcels.fieldset import CGS_field
 
-@pytest.mark.parametrize('x_vel',[1,2,3,4])
-@pytest.mark.parametrize('y_vel',[2,3,4,5])
+@pytest.mark.parametrize('x_vel',[np.array(np.reshape(np.arange(16),[4,4]),dtype=np.float32)])
+@pytest.mark.parametrize('y_vel',[np.array(np.reshape(np.arange(1,17),[4,4]),dtype=np.float32)])
 def test_CGS_fieldset_init(x_vel,y_vel):
     cgs = CGS_field(x_vel,y_vel)
+    cg_u = cgs.fieldset.U.data
+    cg_v = cgs.fieldset.V.data
     
+    assert np.allclose(cg_u,np.array(np.reshape(np.arange(16),[1,4,4]),dtype=np.float32))
+    assert np.allclose(cg_v,np.array(np.reshape(np.arange(1,17),[1,4,4]),dtype=np.float32))
+
+    cg_u_grid = cgs.fieldset.U.grid
+    cg_v_grid = cgs.fieldset.V.grid
+
+    assert np.allclose(cg_u_grid.lon,np.linspace(0.0,1.0,num=4,dtype=np.float32))
+    assert np.allclose(cg_v_grid.lat,np.linspace(0.0,1.0,num=4,dtype=np.float32))
